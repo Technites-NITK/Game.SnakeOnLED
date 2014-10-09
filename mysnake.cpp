49ae46475426d182e1f7d3f00e88a2d3545cc838
akshay <ksh.shrm1@gmail.com>
2014-10-10 02:35:15 +0530

    final code

--- mysnake.cpp
+++ mysnake.cpp
@@ -18,10 +18,13 @@ class Snake
 	Direction direction;
 	int* head;
 	int length;
- int face[8][32];
+  int face[8][32];
+  int x,y,fruit_count,flag;
 	
-  bool isColliding(int head_x,int head_y)
+  bool isColliding(int head_x,int head_y,Direction direction)
   {
+  	//cout<<direction<<endl;
+  	//cout<<"x="<<head_x<<endl<<"y="<<head_y<<endl;
   
          if(direction==RIGHT)
          {
@@ -32,8 +35,10 @@ class Snake
          }
 		 if(direction==LEFT)
 		 {
-		 	if(face[head_y][head_x-1]==1)
+		 	if(face[head_y][head_x-1]==1){
+		 	//	cout<<"head_x-1 is"<<head_x<<endl<<"value is "<<face[head_y][head_x-1]<<endl;
 		  return true;
+	       }
 		  else
 		  return false;
 		 }
@@ -47,7 +52,11 @@ class Snake
 		 if(direction==DOWN)
 		 {
 		 	if(face[head_y+1][head_x]==1)
-		  return true;
+		  {
+		  //	cout<<"Collided!"<<endl<<"x="<<head_x<<endl<<"y="<<head_y+1<<endl;
+		  	//cout<<face[head_y+1][head_x]<<endl;
+		  	return true;
+		  }
 		  else
 		  return false;
 		 } 
@@ -57,21 +66,42 @@ class Snake
   
   void fruit()
   {
-  	int x,y;
+  	
   	x=rand()%32;
   	y=rand()%8;
-  	if(face[y][x]==1)
-  	void fruit();
+  	fruit_count++;
+  	if(fruit_count==1)
+  	{
+  		if(face[y][x]==1)
+  	    fruit();
+  	    else
+  	    face[y][x]=2;
+  	}
+  	
   	else
-  	face[y][x]=2;
+  	{
+  		if(flag)
+  		{
+  				if(face[y][x]==1)
+  	             fruit();
+  	            else
+  	            face[y][x]=2;
+  	            
+  	            flag=1;
+  		}
+  	}
+  	
   }
   
   bool hasEaten()
   {
+  	flag=!flag;
   	if(*SnakeSegment.back()==2)
   	return true;
   	else
   	return false;
+  	
+  	
   }
   
   
@@ -90,7 +120,7 @@ class Snake
 
   	  
   	 
-  	  cout<<"x="<<x<<"y="<<y;
+  	 // cout<<"x="<<x<<"y="<<y;
   	
 	  SnakeSegment.push(face[y]+x);
   	*SnakeSegment.back()=1;
@@ -149,13 +179,13 @@ class Snake
     
 	  	
 	  } 
-	  	for(int k=0;k<8;k++)
+	  	/*for(int k=0;k<8;k++)
   {
   	for(int l=0;l<32;l++)
   	cout<<face[k][l];
   	cout<<endl;
   } 
-	  
+	  */
   
   	length=3;
   	head=SnakeSegment.back();
@@ -170,7 +200,7 @@ class Snake
   	
   }
   
-  void getInput(int head_x)
+ /* void getInput(int head_x)
   {
   	//convert the key input into direction
     if (0<=head_x<=7)
@@ -249,22 +279,45 @@ class Snake
     	
   }
   
-  
+ */ 
   void snakeMove(int head_x,int head_y,Direction direction)
   {
   	int count=0,old_direction;
+  	fruit_count=0;
   	bool go= true;
   	while(go)
   	{
   		
   	
-Sleep ( 1000 );
+Sleep ( 500 );
   		
     old_direction=direction;
   	
-  	getInput(head_x);
+  	//getInput(head_x);
+  	if(kbhit())// check if keyboard key is pressed
+            {
+             switch(getch())
+             {
+             case 'w':direction=UP;
+             if(old_direction==DOWN)
+             direction=DOWN;
+             break;
+             case 's':direction=DOWN;
+             if(old_direction==UP)
+             direction=UP;
+             break; 
+             case 'a':direction=LEFT;
+             if(old_direction==RIGHT)
+             direction=RIGHT;
+             break;
+             case 'd':direction=RIGHT;
+             if(old_direction==LEFT)
+             direction=LEFT;
+             break;	//player 1 control
+  	}
+  }
   	
-  		if(isColliding(head_x,head_y)||(head_y==0&&direction==UP)||(head_y==8&&direction==DOWN))
+  		if(isColliding(head_x,head_y,direction)||(head_y==0&&direction==UP)||(head_y==7&&direction==DOWN))
   	{
   		cout<<"game over";
   		go=false;
@@ -274,8 +327,8 @@ Sleep ( 1000 );
     switch(direction)
     {
     	case UP:
-		   if(old_direction != DOWN )
-		{
+		  // if(old_direction != DOWN )
+		//{
 			
 			SnakeSegment.push(&face[head_y-1][head_x]);
 			
@@ -293,27 +346,9 @@ Sleep ( 1000 );
 		}
 	
 		head_y=head_y-1;
-	}
+//	}
 	
-	else{
-		  SnakeSegment.push(&face[head_y+1][head_x]);
-			if(!hasEaten())
-			{
-			*SnakeSegment.back()=1;
-			*SnakeSegment.front()=0;
-			SnakeSegment.pop();
-		   }
-		  
-		   
-		
-		else
-		{
-			
-			*SnakeSegment.back()=1;
-		}
-	  
-		head_y=head_y+1;
-     }
+
 		break;
 		
 		
@@ -324,8 +359,8 @@ Sleep ( 1000 );
 		
 		case DOWN:
 			
-		      if(old_direction != UP)
-		{
+	//	      if(old_direction != UP)
+	//	{
 			SnakeSegment.push(&face[head_y+1][head_x]);
 			if(!hasEaten())
 			{
@@ -343,28 +378,9 @@ Sleep ( 1000 );
 		}
 	  
 		head_y=head_y+1;
-	  }
+	//  }
 	  
-	  else
-	  {
-	    	SnakeSegment.push(&face[head_y-1][head_x]);
-			
-			if(!hasEaten())
-			{
-			*SnakeSegment.back()=1;
-			*SnakeSegment.front()=0;
-			SnakeSegment.pop();
-	        }
-		
-		else
-		{
-			
-			*SnakeSegment.back()=1;
-		}
-	
-		head_y=head_y-1;
-	}
-		
+
 	  
 		break;
 		
@@ -379,15 +395,17 @@ Sleep ( 1000 );
 		
 		
 		case RIGHT:
-		     if(old_direction != LEFT)
-		{
+	//	     if(old_direction != LEFT)
+	//	{
 			if(head_x==31)
 			{
 				SnakeSegment.push(&face[head_y][0]);
+				head_x=0;
 			}
 			else 
 			{
 				SnakeSegment.push(&face[head_y][head_x+1]);
+				head_x=head_x+1;
 			}
 			if(!hasEaten())
 			{
@@ -404,37 +422,10 @@ Sleep ( 1000 );
 			*SnakeSegment.back()=1;
 		}
 	
-		head_x=head_x+1;
-	}
+		//head_x=head_x+1;
+//	}
 	
-	else{
-		if(head_x==0)
-			{
-				SnakeSegment.push(&face[head_y][31]);
-			}
-			else 
-			{
-				SnakeSegment.push(&face[head_y][head_x-1]);
-			}
-			
-			if(!hasEaten())
-			{
-			
-			*SnakeSegment.back()=1;
-			*SnakeSegment.front()=0;
-			SnakeSegment.pop();
-		    }
-		  
-		
-		else
-		{
-			
-			
-			*SnakeSegment.back()=1;
-		}
-	}
-		head_x=head_x-1;
-	
+
 		break;
 		
 		
@@ -444,15 +435,17 @@ Sleep ( 1000 );
 		
 		
 		case LEFT:
-		  if(old_direction != RIGHT)
-		{
+//		  if(old_direction != RIGHT)
+//		{
 				if(head_x==0)
 			{
 				SnakeSegment.push(&face[head_y][31]);
+				head_x=31;
 			}
 			else 
 			{
 				SnakeSegment.push(&face[head_y][head_x-1]);
+				head_x=head_x-1;
 			}
 			
 			if(!hasEaten())
@@ -471,51 +464,33 @@ Sleep ( 1000 );
 			*SnakeSegment.back()=1;
 		}
 	
-		head_x=head_x-1;
-     }
-     
-     else{
-     	if(head_x==31)
-			{
-				SnakeSegment.push(&face[head_y][0]);
-			}
-			else 
-			{
-				SnakeSegment.push(&face[head_y][head_x+1]);
-			}
-			if(!hasEaten())
-			{
-			*SnakeSegment.back()=1;
-			*SnakeSegment.front()=0;
-			SnakeSegment.pop();
-		   }
-		 
-		
-		else
-		{
-			
-			
-			*SnakeSegment.back()=1;
-		}
 	
-		head_x=head_x+1;
-     }
 		break;
 		
-		
+  }
 		
     
     head=SnakeSegment.back();
     count++;
-  	if(count%5==0)
-  	fruit();
-  }
+  	if(count%20==0)
+  	{
+      fruit();  	
+  	}
+  	
+  
   for(int k=0;k<8;k++)
   {
   	for(int l=0;l<32;l++)
-  	cout<<face[k][l];
+  	{
+  		if(l%8==0)
+  		cout<<"  ";
+  	   cout<<face[k][l];	
+  	}
+  	
   	cout<<endl;
   }
+  
+  cout<<endl<<endl;
 }
 }
 };
